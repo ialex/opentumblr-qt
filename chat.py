19afec54e8fc4cd4392fe9140c980d0f9247aa0b
Alejandro Villanueva <admin@ialex.org>
2009-08-24 11:35:41 -0500

    Adding chat support

--- chat.py
+++ chat.py
@@ -9,11 +9,37 @@ class Chat(Chat_widget):
         self.setupUi()
         self.api = parent.api
         #Conectar eventos
-        self.connect(self.bt_cancel, QtCore.SIGNAL("clicked()"), self.OnCancel)
-        self.connect(self.bt_post, QtCore.SIGNAL("clicked()"), self.OnPost)
+        self.connect(self.bt_cancel, QtCore.SIGNAL('clicked()'), self.OnCancel)
+        self.connect(self.bt_post, QtCore.SIGNAL('clicked()'), self.OnPost)
     
     def OnCancel(self):
         self.close()
     
     def OnPost(self):
-        print "envia esa puta madre "
+        if self.le_title.text().isEmpty():
+            self.title = ''
+        else:
+            self.title = unicode(self.le_title.text()).encode('utf-8')
+	self.conversation = unicode(self.te_chat.toPlainText()).encode('utf-8')
+        if self.advanced.te_tags.toPlainText().isEmpty():
+            self.tags = ''
+        else:
+            self.tags = unicode(self.advanced.te_tags.toPlainText()).encode('utf-8')
+        self.tags = string.replace(self.tags,' ', ',')
+        self.date = self.advanced.le_date.text()
+
+        if self.advanced.cb_publish.currentText() == 'private':
+            self.private = 1
+        else:
+            self.private = 0 
+        
+        if self.conversation:
+            #self.format = None
+            self.api = Api(self.api.name, self.api.email, self.api.password, self.private, self.date, self.tags)
+            try:
+                self.post = self.api.write_conversation(self.title, self.conversation)
+            except:
+                print 'Puta Madre ocurrio un error'
+            self.close()
+        else:
+            QtGui.QMessageBox.warning(self,'Error','Conversation is required',QtGui.QMessageBox.Ok)
