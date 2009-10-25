5d0722781a0d14641aae3121aca106776f64feb3
Alejandro Villanueva <admin@ialex.org>
2009-10-25 08:35:28 -0600

    Fixing Tabs

--- qtumblr/qtumblr-client.py
+++ qtumblr/qtumblr-client.py
@@ -13,43 +13,43 @@ except ImportError:
 errors = {'403':'Login o password incorrectos','404':'Tumblrlog incorrecto','urlopen':'no ingreso su tumblrlog'}
 
 class Cliente_Opentumblr(Main_widget):
-    def __init__(self,parent=None):
-        super(Main_widget, self).__init__(parent)
-        self.setupUi()                             
-        #Conectar eventos
-        QtCore.QObject.connect(self.bt_login, QtCore.SIGNAL('clicked()'), self.OnAuthTumblr)            
+	def __init__(self,parent=None):
+		super(Main_widget, self).__init__(parent)
+		self.setupUi()                             
+		#Conectar eventos
+		print self.bt_login
+		QtCore.QObject.connect(self.bt_login, QtCore.SIGNAL('clicked()'),self.OnAuthTumblr)                
 
-        #Debug properties                                                                      
-        self.le_mail.setText('admin@ialex.org')                                                
-        self.le_url.setText('http://ialex.tumblr.com')
+		#Debug properties                                                                      
+		self.le_mail.setText('admin@ialex.org')                                                
+		self.le_url.setText('http://ialex.tumblr.com')
 
-        def OnAuthTumblr(self):
-            self.User = self.le_mail.text()                
-            self.Password = self.le_password.text()
-            self.Blog = self.le_url.text()	
-            self.error = None
-            if not self.User.isEmpty() | self.Password.isEmpty() | self.Blog.isEmpty():
-                self.api = Api(self.Blog,self.User,self.Password)
+	def OnAuthTumblr(self):
+		self.User = self.le_mail.text()                
+		self.Password = self.le_password.text()
+		self.Blog = self.le_url.text()	
+		self.error = None
+		if not self.User.isEmpty() | self.Password.isEmpty() | self.Blog.isEmpty():
+			self.api = Api(self.Blog,self.User,self.Password)
 
-                try:
-                    self.auth = self.api.auth_check()
-                    #Abrir la ventana del dashboard
-                    dashboard = Dashboard(self)                                                                  
-                    self.hide()
-                    dashboard.show()                                
-                    #print 'Te haz logueado'
-                except TumblrAuthError:	    		
-                    self.error = errors['403']
-                except urllib2.HTTPError:
-                    self.error = errors['404']
-                except urllib2.URLError:
-                    self.error = errors['urlopen']
-                finally:
-                    if self.error != None:
-                        QtGui.QMessageBox.warning(self,'Error','Occurrio un error: \n' + self.error,QtGui.QMessageBox.Ok)			    
-                    else:
-                        QtGui.QMessageBox.warning(self,'Error','Todos los Campos son necesarios',QtGui.QMessageBox.Ok)
-
+			try:
+				self.auth = self.api.auth_check()
+				#Abrir la ventana del dashboard
+				dashboard = Dashboard(self)                                                                  
+				self.hide()
+				dashboard.show()                                
+				#print 'Te haz logueado'
+			except TumblrAuthError:	    		
+				self.error = errors['403']
+			except urllib2.HTTPError:
+				self.error = errors['404']
+			except urllib2.URLError:
+				self.error = errors['urlopen']
+			finally:
+				if self.error != None:
+					QtGui.QMessageBox.warning(self,'Error','Occurrio un error: \n' + self.error,QtGui.QMessageBox.Ok)			    
+				else:
+					QtGui.QMessageBox.warning(self,'Error','Todos los Campos son necesarios',QtGui.QMessageBox.Ok)		
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
