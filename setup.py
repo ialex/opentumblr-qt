14359cf1fc05d1c75f61f472857b6fbce3aad892
Alejandro Villanueva <admin@ialex.org>
2009-11-05 20:35:48 -0600

    Removing dependency to setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, find_packages
+#!/usr/bin/python
+from distutils.core import setup
 import os, sys
 
 packages_path = os.path.abspath(os.path.dirname(__file__))
@@ -9,15 +10,16 @@ icon_files = []
 image_files = []
 
 prefix = sys.prefix + '/'
-qtumblr = packages_path + '/qtumblr'
+qtumblr = packages_path + '/opentumblrqt'
+print qtumblr 
 
 for arg in sys.argv:
 	if arg.startswith('--prefix='):
 		prefix = arg[9:]
 		prefix = os.path.expandvars(prefix)
 
-ipath_docs = '%sshare/doc/qtumblr/' % prefix
-ipath_images = '%sshare/pixmaps/qtumblr/' % prefix
+ipath_docs = '%sshare/doc/opentumblr-qt/' % prefix
+ipath_images = '%sshare/pixmaps/opentumblr-qt/' % prefix
 ipath_icons = '%sshare/pixmaps/' % prefix
 ipath_desktop_file = '%sshare/applications/' % prefix
 ipath_dashboard = ipath_images+'dashboard/'
@@ -28,11 +30,11 @@ images = ['audio.png','chat.png','link.png','photo.png','quote.png','text.png','
 for img in images:
 	image_files.append(path_images + img)
 
-docs = ['AUTHORS','INSTALL','LICENSE','README','THANKS','PKG-INFO']
+docs = ['AUTHORS','INSTALL','LICENSE','README','THANKS']
 for doc in docs:
 	doc_files.append(packages_path + '/' + doc)
 
-icons = ['qtumblr.png','qtumblr.xpm']
+icons = ['opentumblr-qt.png','opentumblr-qt.xpm']
 for icon in icons:
 	icon_files.append(path_images + icon)
 
@@ -50,11 +52,11 @@ if not os.path.isdir(ipath_images):
 
 datafiles.append((ipath_icons, icon_files))
 datafiles.append((ipath_docs, doc_files))
-datafiles.append((ipath_desktop_file,[packages_path + '/qtumblr.desktop']))
+datafiles.append((ipath_desktop_file,[packages_path + '/opentumblr-qt.desktop']))
 datafiles.append((ipath_dashboard, image_files))
 
 setup(
-    name = 'Qtumblr',
+    name = 'opentumblr-qt',
     version = '0.1',
     description = 'Qt Tumblr Client based on Opentumblr',
     author = 'Alejandro Villanueva',
@@ -62,11 +64,11 @@ setup(
     url = 'http://ialex.org',
     license = 'MIT',
     scripts = ['opentumblrqt/opentumblr-qt-client.py'],
-    packages = find_packages(),
-    #package_dir = {'qtumblr' : qtumblr,'gui' : gui,}, 
+    packages = ['opentumblrqt','opentumblrqt.gui'],
+    #package_dir = {'qtumblr' : tumblr,}, 
     data_files = datafiles,
-    include_package_data = True,
-    install_requires = ['poster','simplejson']
+    #include_package_data = True,
+    #install_requires = ['poster','simplejson']
      )
-print datafiles
-print packages_path
+#print datafiles
+#print packages_path
