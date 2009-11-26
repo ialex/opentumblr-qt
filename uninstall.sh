5797dd2faca86d568cb72e51829d8164e8bedf48
root <root@littlebird.(none)>
2009-11-25 18:16:54 -0600

    Adding a Really Simple Uninstaller

---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+rm -r /usr/share/doc/opentumblr-qt
+rm -r /usr/share/pixmaps/opentumblr-qt
+rm /usr/share/pixmaps/opentumblr-qt.png
+rm /usr/share/pixmaps/opentumblr-qt.xpm
+rm -r /usr/lib/python2.5/site-packages/opentumblrqt
+rm /usr/bin/opentumblr-qt-client.py
